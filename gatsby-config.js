d2a9ba14366110629b8a0150f808f30c9cf80970
woosuk Kim <woosukkim@woosukui-MacBookPro.local>
2020-01-26 23:21:52 +0900

    trackingId change

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,7 +124,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-43243939-2' },
+      options: { trackingId: 'UA-125954651-1' },
     },
     {
       resolve: 'gatsby-plugin-google-fonts',
