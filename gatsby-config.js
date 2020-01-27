743214914dcc3c7f3c22d9a1be3c343be8f86e08
woosuk Kim <woosukkim@woosukui-MacBookPro.local>
2020-01-27 20:42:37 +0900

    change commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,14 +17,6 @@ module.exports = {
         label: 'Articles',
         path: '/',
       },
-      // {
-      //   label: 'About me',
-      //   path: '/about/',
-      // },
-      // {
-      //   label: 'Contact me',
-      //   path: '/contact/',
-      // },
     ],
     author: {
       name: 'IPADORUSA',
