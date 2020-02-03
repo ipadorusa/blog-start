5e6b6974bae28aaed77e791a411870b5d38d4f1f
woosuk Kim <woosukkim@woosukui-MacBookPro.local>
2020-02-03 22:23:46 +0900

    seo change

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     subtitle: '웹프론트엔드를 꿈꾼다',
     copyright: '© All rights reserved.',
     disqusShortname: 'ipadorusa-com',
+    image: './photo.jpg',
     menu: [
       {
         label: 'Articles',
@@ -22,7 +23,7 @@ module.exports = {
       name: 'IPADORUSA',
       email: 'ipadorusa@gmail.com',
       telegram: '#',
-      twitter: '//twitter.com/kedokedobookedo',
+      twitter: '@kedokedobookedo',
       github: '//github.com/ipadorusa',
       rss: '#',
       vk: '#',
