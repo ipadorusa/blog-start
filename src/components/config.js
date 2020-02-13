baf8faf63a882115547b020e89bba24abd287f95
rainguy1 <rainguy1@saramin.co.kr>
2020-02-13 10:20:50 +0900

    delete

--- src/components/config.js
+++
@@ -1,8 +0,0 @@
-const Config = {
-  title: 'Khalil Stemmler - Software Developer / Designer',
-  twitter: 'https://twitter.com/kedokedo',
-  url: 'https://ipadorusa.com',
-  logo: 'https://ipadorusa.com/photo.jpg',
-}
-
-export default Config
